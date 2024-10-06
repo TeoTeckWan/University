0736b8d273ff3c5757f84d36d2084f758e349bbf
Teckwann <teoteck0827@gmail.com>
2024-10-06 22:12:10 +0800

    Delete Appium-Project directory

--- Appium-Project/backup.py
+++
@@ -1,9 +0,0 @@
-import json
-
-# Load JSON data from a file
-with open('database.json', 'r') as file:
-    data = json.load(file)
-
-    for user in data["users"]:
-        if user["personName"] == "TeckWan":
-            print(f'{user["number"]},{user["code"]}')
